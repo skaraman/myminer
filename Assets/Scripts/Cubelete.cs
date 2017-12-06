83101b73017ef604cf772ae8e8fbede178a2ff2e
Sem K <sem@Sems-MacBook-Pro.local>
2017-12-06 07:50:14 -0800

    cubelete show/hide loop

---
+++ Assets/Scripts/Cubelete.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubelete : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        transform.Rotate(0, Time.deltaTime * turnSpeed, 0);
+    }
+
+    void OnMouseDown()
+    {
+        gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/TheCube.cs
+++ Assets/Scripts/TheCube.cs
@@ -3,155 +3,135 @@ using UnityEngine;
 
 public class TheCube : MonoBehaviour
 {
-    public GameObject cubelete;
-    public GameObject surface;
-    public GameObject fakeSurface;
-    public GameObject fakeVolume;
-    public float spawnMinDist;
-    public float yMax;
-    public float xMax;
-    public float zMax;
+	public GameObject cubelete;
+	public GameObject surface;
+	public GameObject fakeSurface;
+	public GameObject fakeVolume;
+	public float spawnMinDist;
+	public float yMax;
+	public float xMax;
+	public float zMax;
 
-    Vector3 startPOS;
+	Vector3 cubeleteBasePosition;
+	Vector3 cubeTrackPosition;
 
-    private bool faded = false;
-    private bool tweaked = false;
+	private bool faded = false;
+	private bool tweaked = false;
 
-    //private Data data = new Data ();
-    //private string cubedata;   
+	//private Data data = new Data ();
+	//private string cubedata;
 
-    void Start()
-    {
-        startPOS = cubelete.transform.localPosition;
-    }
+	void Start ()
+	{
+		cubeleteBasePosition = cubelete.transform.localPosition;
+		cubeTrackPosition = transform.localPosition;
+	}
 
-    void Update()
-    {
-        Vector3 currPos = transform.localPosition;
+	void Update ()
+	{
+		Vector3 currPos = transform.localPosition;
 
-        if (currPos.z > 0.51 && faded == false)
-        {
-            FadeCube(true);
-        }
-        else
-        if (currPos.z <= 0.51 && faded == true)
-        {
-            FadeCube(false);
-        }
-        //else
-        //if (currPos.z < 0.3 && tweaked == false)
-        //{
-        //    TweakSurface(true);
-        //}
-        //else
-        //if (currPos.z >= 0.3 && tweaked == true)
-        //{
-        //    TweakSurface(false);
-        //}
-    }
-
-    void FadeCube(bool fade)
-    {
-        faded = fade;
-        fakeSurface.SetActive(fade);
-        surface.SetActive(!fade);
-    }
-
-    void TweakSurface(bool tweak)
-    {
-        tweaked = tweak;
-        Component[] children = surface.GetComponentsInChildren<Transform>();
-        float it;
-        foreach (Transform child in children)
-        {
-            float newX = 0.0f;
-            float newY = 0.0f;
-
-            if (child.localPosition.x > 0)
-            {
-                it = child.localPosition.x / 0.02f;
-                newX = Operator(child.localPosition.x, (0.001f * it), tweak);
-            }
-            if (child.localPosition.y > 0)
-            {
-                it = child.localPosition.y / 0.02f;
-                newY = Operator(child.localPosition.y, (0.001f * it), tweak);
-            }
-            //iTween.MoveBy(child.gameObject, iTween.Hash("x", newX, "y", newY, "easeType", "easeInOutExpo", "delay", .1));
-
-            child.localPosition = new Vector3(newX, newY, child.localPosition.z);
-        }
-    }
-
-    public void MakeInteractibleSurface()
-    {
-        var parentX = gameObject.transform.position.x;
-        var parentY = gameObject.transform.position.y;
-        // every 0.02 of parent position is 1 cubelete width/height
-        // so i have to create all cubeletes based on current parent position
+		if (currPos.z > 0.51 && faded == false) {
+			FadeCube (true);
+		} else if (currPos.z <= 0.51 && faded == true) {
+			FadeCube (false);
+		}
 
 
-        for (int i = 0; i < xMax; i++)
-        {
-            for (int j = 0; j < yMax; j++)
-            {
-                for (int k = 0; k < zMax; k++)
-                {
-                    GameObject newCubelete = Instantiate(cubelete, surface.transform);
-                    newCubelete.transform.localPosition = new Vector3(
-                        startPOS.x + (spawnMinDist * i),
-                        startPOS.y + (spawnMinDist * j),
-                        startPOS.z + (spawnMinDist * k)
-                    );
-                }
-            }
-        }
-    }
+//	 else if (currPos.z < 0.3 && tweaked == false) {
+//			TweakSurface (true);
+//		} else if (currPos.z >= 0.3 && tweaked == true) {
+//			TweakSurface (false);
+//		}
+	}
 
-    //public void MakeInteractibleSurface(int[][][] data)
-    //{
-    //    for (int i = 0; i < xMax; i++)
-    //    {
-    //        for (int j = 0; j < yMax; j++)
-    //        {
-    //            for (int k = 0; k < zMax; k++)
-    //            {
-    //                if (data[i][j][k] == 1)
-    //                {
-    //                    GameObject newCubelete = Instantiate(cubelete, surface.transform);
-    //                    newCubelete.transform.localPosition = new Vector3(
-    //                        startPOS.x + (spawnMinDist * i),
-    //                        startPOS.y + (spawnMinDist * j),
-    //                        startPOS.z + (spawnMinDist * k)
-    //                    );
-    //                }
-    //            }
-    //        }
-    //    }
-    //}
+	void FadeCube (bool fade)
+	{
+		faded = fade;
+		fakeSurface.SetActive (fade);
+		surface.SetActive (!fade);
+	}
 
-    //public void SaveData ()
-    //{
-    //  string json = data.GetJsonFromCurrentCube (surface.GetComponentsInChildren<Transform> ());
-    //  data.SaveData (json);
-    //}
+	void TweakSurface (bool tweak)
+	{
+//		tweaked = tweak;
+//		var children = surface.GetComponentsInChildren<Transform> ();
+//		for (var c = 1; c < children.Length; c++) {
+//			var child = children [c];
+//
+//			float newX = 0.0f;
+//			float newY = -0.38f;
+//			float spacer;
+//			if (tweak) {
+//				spacer = 0.02f;
+//			} else {
+//				spacer = 0.04f;
+//			}
+//			int xit = Mathf.RoundToInt (Mathf.Abs (child.localPosition.x / spacer));
+//			int yit = Mathf.RoundToInt (Mathf.Abs ((child.localPosition.y + 0.38f) / spacer));
+//
+//			if (child.localPosition.x > 0) {
+//				newX = Operator (child.localPosition.x, 0.02f * xit, tweak);
+//			} else if (child.localPosition.x < 0) {
+//				newX = Operator (child.localPosition.x, 0.02f * xit, !tweak);
+//			}
+//			if (child.localPosition.y + 0.38f > 0) {
+//				newY = Operator (child.localPosition.y, 0.02f * yit, tweak);
+//			} else if (child.localPosition.y + 0.38f < 0) {
+//				newY = Operator (child.localPosition.y, 0.02f * yit, !tweak);
+//			}
+//			child.localPosition = new Vector3 (newX, newY, child.localPosition.z);
+//		}
 
-    //public void LoadData ()
-    //{
-    //  cubedata = data.LoadData ();
-    //}
+	}
 
-    static float Operator(float x, float multiplier, bool op)
-    {
-        float results = 0;
-        if (op == true)
-        {
-            results = x + multiplier;
-        }
-        else if (op == false)
-        {
-            results = x - multiplier;
-        }
-        return results;
-    }
+	public void MakeInteractibleSurface ()
+	{
+		var parentX = gameObject.transform.position.x;
+		var parentY = gameObject.transform.position.y;
+		// every 0.02 of parent position is 1 cubelete width/height
+		// so i have to create all cubeletes based on current parent position
+
+		var xStart = -xMax / 2;
+		var yStart = -yMax / 2;
+
+		for (int i = 0; i < xMax; i++) {
+			for (int j = 0; j < yMax; j++) {
+				for (int k = 0; k < zMax; k++) {
+					GameObject newCubelete = Instantiate (cubelete, surface.transform);
+					newCubelete.transform.localPosition = new Vector3 (
+						cubeleteBasePosition.x + (spawnMinDist * xStart),
+						cubeleteBasePosition.y + (spawnMinDist * yStart) - 0.38f,
+						cubeleteBasePosition.z + (spawnMinDist * k)
+					);
+				}
+				yStart++;
+			}
+			xStart++;
+			yStart = -yMax / 2;
+		}
+	}
+
+	//public void SaveData ()
+	//{
+	//  string json = data.GetJsonFromCurrentCube (surface.GetComponentsInChildren<Transform> ());
+	//  data.SaveData (json);
+	//}
+
+	//public void LoadData ()
+	//{
+	//  cubedata = data.LoadData ();
+	//}
+
+
+	static float Operator (float x, float multiplier, bool op)
+	{
+		float results = 0;
+		if (op == true) {
+			results = x + multiplier;
+		} else if (op == false) {
+			results = x - multiplier;
+		}
+		return results;
+	}
 }
