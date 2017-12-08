b2091f5300c52fc94236150114349b86e35961e3
Sem K <sem@Sems-MacBook-Pro.local>
2017-12-08 07:54:39 -0800

    cubelete show/hide loop cont'

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -1,37 +1,47 @@
 using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
+
 public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    public static GameObject DraggedInstance;
-    Vector3 _startPosition;
-    Vector3 _offsetToMouse;
-    float _zDistanceToCamera;
-    Vector3 temp;
+	public static GameObject DraggedInstance;
+	Vector3 _startPosition;
+	Vector3 _offsetToMouse;
+	float _zDistanceToCamera;
+	Vector3 temp;
+	public TheCube cube;
 
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        DraggedInstance = gameObject;
-        _startPosition = transform.position;
-        _zDistanceToCamera = Mathf.Abs(_startPosition.z - Camera.main.transform.position.z);
-        _offsetToMouse = _startPosition - Camera.main.ScreenToWorldPoint(
-            new Vector3(Input.mousePosition.x, Input.mousePosition.y, _zDistanceToCamera)
-        );
-        _offsetToMouse.z = 0;
-    }
-    public void OnDrag(PointerEventData eventData)
-    {
-        if (Input.touchCount > 1)
-            return;
-        temp = Camera.main.ScreenToWorldPoint(
-            new Vector3(Input.mousePosition.x, Input.mousePosition.y, _startPosition.z)
-        ) + _offsetToMouse;
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		DraggedInstance = gameObject;
+		_startPosition = transform.position;
+		_zDistanceToCamera = Mathf.Abs (_startPosition.z - Camera.main.transform.position.z);
+		_offsetToMouse = _startPosition - Camera.main.ScreenToWorldPoint (
+			new Vector3 (Input.mousePosition.x, Input.mousePosition.y, _zDistanceToCamera)
+		);
+		_offsetToMouse.z = 0;
+	}
 
-        transform.position = new Vector3(temp.x, temp.y, _startPosition.z);
-    }
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        DraggedInstance = null;
-        _offsetToMouse = Vector3.zero;
-    }
+	public void OnDrag (PointerEventData eventData)
+	{
+		if (Input.touchCount > 1)
+			return;
+		temp = Camera.main.ScreenToWorldPoint (
+			new Vector3 (Input.mousePosition.x, Input.mousePosition.y, _startPosition.z)
+		) + _offsetToMouse;
+		//var savedPosition = transform.position;
+		transform.position = new Vector3 (temp.x, temp.y, _startPosition.z);
+//		var xCubes = Mathf.RoundToInt ((savedPosition.x - temp.x) / 0.02f);
+//		var yCubes = Mathf.RoundToInt ((savedPosition.y - temp.y) / 0.02f);
+//		if (xCubes > 0 || yCubes > 0) {
+//			cube.UpdateInteractibleSurface (xCubes, yCubes);
+//		}
+
+	}
+
+	public void OnEndDrag (PointerEventData eventData)
+	{
+		DraggedInstance = null;
+		_offsetToMouse = Vector3.zero;
+	}
 }
--- Assets/Scripts/TheCube.cs
+++ Assets/Scripts/TheCube.cs
@@ -17,6 +17,10 @@ public class TheCube : MonoBehaviour
 
 	private bool faded = false;
 	private bool tweaked = false;
+	private float xTracker;
+	private float yTracker;
+	private float xCubes;
+	private float yCubes;
 
 	//private Data data = new Data ();
 	//private string cubedata;
@@ -37,12 +41,26 @@ public class TheCube : MonoBehaviour
 			FadeCube (false);
 		}
 
+		xTracker += (currPos.x - cubeTrackPosition.x);
+		yTracker += (currPos.y - cubeTrackPosition.y);
+		cubeTrackPosition = currPos;
 
-//	 else if (currPos.z < 0.3 && tweaked == false) {
-//			TweakSurface (true);
-//		} else if (currPos.z >= 0.3 && tweaked == true) {
-//			TweakSurface (false);
-//		}
+		xCubes = xTracker / 0.02f;
+		yCubes = yTracker / 0.02f;
+
+		if (Mathf.Abs (xCubes) > 1 || Mathf.Abs (yCubes) > 1) {
+			var xTakeaway = Mathf.Round (xCubes);
+			var yTakeaway = Mathf.Round (yCubes);
+			xTracker -= xTakeaway * 0.02f;
+			yTracker -= yTakeaway * 0.02f;
+			UpdateInteractibleSurface (xTakeaway, yTakeaway);
+		}
+
+		//	 else if (currPos.z < 0.3 && tweaked == false) {
+		//			TweakSurface (true);
+		//		} else if (currPos.z >= 0.3 && tweaked == true) {
+		//			TweakSurface (false);
+		//		}
 	}
 
 	void FadeCube (bool fade)
@@ -54,41 +72,41 @@ public class TheCube : MonoBehaviour
 
 	void TweakSurface (bool tweak)
 	{
-//		tweaked = tweak;
-//		var children = surface.GetComponentsInChildren<Transform> ();
-//		for (var c = 1; c < children.Length; c++) {
-//			var child = children [c];
-//
-//			float newX = 0.0f;
-//			float newY = -0.38f;
-//			float spacer;
-//			if (tweak) {
-//				spacer = 0.02f;
-//			} else {
-//				spacer = 0.04f;
-//			}
-//			int xit = Mathf.RoundToInt (Mathf.Abs (child.localPosition.x / spacer));
-//			int yit = Mathf.RoundToInt (Mathf.Abs ((child.localPosition.y + 0.38f) / spacer));
-//
-//			if (child.localPosition.x > 0) {
-//				newX = Operator (child.localPosition.x, 0.02f * xit, tweak);
-//			} else if (child.localPosition.x < 0) {
-//				newX = Operator (child.localPosition.x, 0.02f * xit, !tweak);
-//			}
-//			if (child.localPosition.y + 0.38f > 0) {
-//				newY = Operator (child.localPosition.y, 0.02f * yit, tweak);
-//			} else if (child.localPosition.y + 0.38f < 0) {
-//				newY = Operator (child.localPosition.y, 0.02f * yit, !tweak);
-//			}
-//			child.localPosition = new Vector3 (newX, newY, child.localPosition.z);
-//		}
+		//		tweaked = tweak;
+		//		var children = surface.GetComponentsInChildren<Transform> ();x
+		//		for (var c = 1; c < children.Length; c++) {
+		//			var child = children [c];
+		//
+		//			float newX = 0.0f;
+		//			float newY = -0.38f;
+		//			float spacer;
+		//			if (tweak) {
+		//				spacer = 0.02f;
+		//			} else {
+		//				spacer = 0.04f;
+		//			}
+		//			int xit = Mathf.RoundToInt (Mathf.Abs (child.localPosition.x / spacer));
+		//			int yit = Mathf.RoundToInt (Mathf.Abs ((child.localPosition.y + 0.38f) / spacer));
+		//
+		//			if (child.localPosition.x > 0) {
+		//				newX = Operator (child.localPosition.x, 0.02f * xit, tweak);
+		//			} else if (child.localPosition.x < 0) {
+		//				newX = Operator (child.localPosition.x, 0.02f * xit, !tweak);
+		//			}
+		//			if (child.localPosition.y + 0.38f > 0) {
+		//				newY = Operator (child.localPosition.y, 0.02f * yit, tweak);
+		//			} else if (child.localPosition.y + 0.38f < 0) {
+		//				newY = Operator (child.localPosition.y, 0.02f * yit, !tweak);
+		//			}
+		//			child.localPosition = new Vector3 (newX, newY, child.localPosition.z);
+		//		}
 
 	}
 
 	public void MakeInteractibleSurface ()
 	{
-		var parentX = gameObject.transform.position.x;
-		var parentY = gameObject.transform.position.y;
+		var parentX = transform.position.x;
+		var parentY = transform.position.y;
 		// every 0.02 of parent position is 1 cubelete width/height
 		// so i have to create all cubeletes based on current parent position
 
@@ -110,6 +128,13 @@ public class TheCube : MonoBehaviour
 			xStart++;
 			yStart = -yMax / 2;
 		}
+	}
+
+	public void UpdateInteractibleSurface (float xCubes, float yCubes)
+	{
+		Debug.Log (string.Format ("xcubes: {0} ycubes: {1}", xCubes, yCubes));
+		Debug.Log (string.Format ("current Cube: {0},{1}", transform.localPosition.x, transform.localPosition.y));
+
 	}
 
 	//public void SaveData ()
